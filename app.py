4e4038b6ac0a7072e7ce04e9fde0a37ad51e7fb4
conjohnson712 <conjohnson712@gmail.com>
2022-07-27 13:59:33 -0400

    Implemented artist search functionality

--- app.py
+++ app.py
@@ -246,16 +246,18 @@ def artists():
 
 @app.route('/artists/search', methods=['POST'])
 def search_artists():
-  # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-  # search for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
-  # search for "band" should return "The Wild Sax Band".
-  response={
-    "count": 1,
-    "data": [{
-      "id": 4,
-      "name": "Guns N Petals",
-      "num_upcoming_shows": 0,
-    }]
+  # Reference: https://knowledge.udacity.com/questions/711360
+  search = request.form.get('search_term', '')
+  artists = Artist.query.filter(Artist.name.ilike("%" + search + "%")).all()
+  data = []
+  for artist in artists:
+      data.append({
+          'id': artist.id,
+          'name': artist.name,
+      })
+  response = {
+      "count": len(artists),
+      "data": data
   }
   return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 
