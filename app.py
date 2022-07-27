fedb8f5e4361f0515151fcbfa380fcde99989fd7
conjohnson712 <conjohnson712@gmail.com>
2022-07-27 13:07:44 -0400

    Implemented Venue Update functionality

--- app.py
+++ app.py
@@ -270,13 +270,10 @@ def show_artist(artist_id):
 def edit_artist(artist_id):
   artist = Artist.query.filter(Artist.id == artist_id).first()
   form = ArtistForm(obj=artist)
-  # TODO: populate form with fields from artist with ID <artist_id>
   return render_template('forms/edit_artist.html', form=form, artist=artist)
 
 @app.route('/artists/<int:artist_id>/edit', methods=['POST'])
 def edit_artist_submission(artist_id):
-  # TODO: take values from the form submitted, and update existing
-  # artist record with ID <artist_id> using the new attributes
   form = ArtistForm(request.form)
   try:
       artist = Artist.query.filter(Artist.id == artist_id).first()
@@ -293,28 +290,24 @@ def edit_artist_submission(artist_id):
 
 @app.route('/venues/<int:venue_id>/edit', methods=['GET'])
 def edit_venue(venue_id):
-  form = VenueForm()
-  venue={
-    "id": 1,
-    "name": "The Musical Hop",
-    "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-    "address": "1015 Folsom Street",
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "123-123-1234",
-    "website": "https://www.themusicalhop.com",
-    "facebook_link": "https://www.facebook.com/TheMusicalHop",
-    "seeking_talent": True,
-    "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-    "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
-  }
-  # TODO: populate form with values from venue with ID <venue_id>
+  venue= Venue.query.filter(Venue.id == venue_id).first()
+  form = VenueForm(obj=venue)
+  
   return render_template('forms/edit_venue.html', form=form, venue=venue)
 
 @app.route('/venues/<int:venue_id>/edit', methods=['POST'])
 def edit_venue_submission(venue_id):
-  # TODO: take values from the form submitted, and update existing
-  # venue record with ID <venue_id> using the new attributes
+  form = VenueForm(request.form)
+  try:
+      venue = Venue.query.filter(Venue.id == venue_id).first()
+      form.populate_obj(venue)
+      db.session.commit()
+      flash(f'Venue {form.name.data} was successfully edited!')
+  except ValueError as e:
+      db.session.rollback()
+      flash(f'An error occurred in {form.name.data}. Error: {str(e)}')
+  finally:
+      db.session.close()
   return redirect(url_for('show_venue', venue_id=venue_id))
 
 #  Create Artist
